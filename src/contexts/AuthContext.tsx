6af5059ecc70b3a5e62d4856aa5452c7df5a035c
Lucas Santana <ln.santana08@gmail.com>
2022-09-21 12:36:41 -0300

    Realizando Login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { createContext } from "react";
+import jwtDecode, { JwtPayload } from "jwt-decode";
+import { createContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface AuthContextProviderProps {
   children: JSX.Element;
@@ -10,17 +12,75 @@ interface RegisterProps {
   password: string;
 }
 
+interface LoginProps {
+  username: string;
+  password: string;
+}
+
+interface UserProps {
+  userID: string;
+  username: string;
+}
+
 interface AuthContextProps {
+  handleLogin: (props: LoginProps) => Promise<void>;
   handleRegister: (props: RegisterProps) => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState<UserProps>();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function validateToken(token: string): UserProps {
+    const decodedToken = jwtDecode<JwtPayload & UserProps>(token);
+    if (!decodedToken.exp || decodedToken.exp <= Date.now() / 1000) {
+      throw Error();
+    }
+
+    return {
+      userID: decodedToken.userID,
+      username: decodedToken.username
+    }
+  }
+
+  async function handleLogin({ username, password }: LoginProps) {
+    const { data } = await axios.post<{ token: string }>('http://localhost:3333/auth/signin', { username, password });
+    const user = validateToken(data.token);
+    setUser(user);
+    setIsLogged(true);
+    localStorage.setItem("token", data.token);
+  }
 
   async function handleRegister({ username, password }: RegisterProps) {
     await axios.post('http://localhost:3333/user/signup', { username, password });
   }
 
-  return <AuthContext.Provider value={{ handleRegister }}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    try {
+      const localToken = localStorage.getItem('token');
+      if (!localToken) {
+        throw new Error();
+      }
+      const user = validateToken(localToken);
+      setUser(user);
+      setIsLogged(true);
+    } catch (error) {
+      setIsLogged(false);
+      localStorage.removeItem('token');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLogged) {
+      if (['/entrar', '/cadastrar'].includes(location.pathname)) {
+        navigate('/');
+      }
+    }
+  }, [isLogged]);
+
+  return <AuthContext.Provider value={{ handleLogin, handleRegister }}>{children}</AuthContext.Provider>;
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
-import { Button, Divider, Flex, FormControl, FormLabel, Heading, Input, Text } from "@chakra-ui/react";
+import { Button, Divider, Flex, FormControl, FormLabel, Heading, Input, Text, useToast } from "@chakra-ui/react";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 interface FormData {
   username: string;
@@ -9,12 +11,58 @@ interface FormData {
 
 export function LoginPage(): JSX.Element {
   const { register, handleSubmit } = useForm<FormData>();
+  const toast = useToast();
+  const { handleLogin } = useAuth();
+
+  async function onSubmitClick({ username, password }: FormData) {
+    if (!username || !password) {
+      toast({
+        status: 'error',
+        title: "Opa!",
+        description: "Todos os campos são obrigatórios.",
+        isClosable: true,
+        duration: 3000
+      });
+      return;
+    }
+
+    try {
+      await handleLogin({ username, password });
+      toast({
+        status: 'success',
+        title: "Oba!",
+        description: "Usuário logado com sucesso.",
+        isClosable: true,
+        duration: 3000
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const response = error.response?.data as unknown as { error: string };
+        toast({
+          title: "Opa!",
+          status: "error",
+          description: response.error ?? 'Não foi possível continuar. Tente novamente.',
+          isClosable: true,
+          duration: 3000
+        });
+        return;
+      }
+
+      toast({
+        title: "Opa!",
+        status: "error",
+        description: 'Não foi possível continuar. Tente novamente.',
+        isClosable: true,
+        duration: 3000
+      });
+    }
+  }
 
   return (
     <Flex w='100%' h='100vh' align='center' justify='center'>
       <Flex bg='white' p='1.5rem' borderRadius='lg' boxShadow='base' w='25rem' h='30rem' direction='column' gap='1rem'>
         <Heading>Entrar</Heading>
-        <Flex as='form' gap='0.5rem' direction='column' onSubmit={handleSubmit(() => { })} grow={1} justify='center'>
+        <Flex as='form' gap='0.5rem' direction='column' onSubmit={handleSubmit(onSubmitClick)} grow={1} justify='center'>
           <FormControl>
             <FormLabel>Nome de Usuário</FormLabel>
             <Input type='text' {...register('username')} />
